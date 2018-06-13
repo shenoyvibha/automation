5e833bb375064bde35203904b801847d9da4d465
shenoyvibha <31980157+shenoyvibha@users.noreply.github.com>
2018-06-13 13:27:08 +0530

    Add files via upload

--- newMethods.java
+++ newMethods.java
@@ -1,12 +1,17 @@
 package com.pack.test;
 
 import com.pack.helper.baseClass;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidKeyCode;
+import javafx.scene.input.KeyCode;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
+
+import static org.openqa.selenium.Keys.ENTER;
 
 public class newMethods {
     public newMethods() throws MalformedURLException {
@@ -24,20 +29,38 @@ public class newMethods {
     }
 
     @Test(priority = 2, description = "When User enters a valid repository and clicks on search, "+
-                                      "Then the list of errors for that repository should be dispalyed")
-    public void validSearch() {
+                                      "Then the list of errors for that repository should be dispalyed, scroll through that list")
+    public void validSearch() throws InterruptedException {
         baseClass.searchBarClick();
-        baseClass.sendKeysToSearchBar("square/retrofit");
-        ((AndroidDriver)baseClass.driver).pressKeyCode(AndroidKeyCode.KEYCODE_SEARCH);
+        baseClass.sendKeysToSearchBar("GoodieBag/CarouselPicker"); //GoodieBag/CarouselPicker
+       //((AndroidDriver)baseClass.driver).pressKeyCode(AndroidKeyCode.KEYCODE_ENTER);
+       Thread.sleep(3000);
+        baseClass.driver.findElementByXPath("//android.widget.TextView[@text='a way to change alpha of not \"focused/chosen\" items?']").click();
+        ((AndroidDriver) baseClass.driver).findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Color\"));");
+    }
 
+    @Test(priority = 3, description = "When User enters a valid repository and clicks on search," +
+                                      "Then the list of errors for that repo should be listed and an issue should have Issue title, issue Id, creator of the issue ")
+    public void validSearchFields() throws InterruptedException {
+        baseClass.searchBarClick();
+        baseClass.sendKeysToSearchBar("GoodieBag/CarouselPicker");
+        Thread.sleep(5000);
+        baseClass.getIssueCreatorField().isDisplayed();
+        baseClass.getIssueTitle().isDisplayed();
+        baseClass.getIssueIdNumber().isDisplayed();
+        baseClass.getIssueIdHolder().isDisplayed();
+        baseClass.getIssueCreatorField().isDisplayed();
+        baseClass.getIssueCreatorName().isDisplayed();
+        Assert.assertEquals(baseClass.getIssueIdHolder().getText(),"id: ");
+        Assert.assertEquals(baseClass.getIssueCreatorField().getText(),"creator: ");
 
     }
-    @Test(priority = 3, description = "When User enter an invalid repository name and clicks on search"+
+    @Test(priority = 6, description = "When User enter an invalid repository name and clicks on search"+
                                       "Then the error with the message 'Oops! some error occured' should appear")
     public void invalidSearch() throws MalformedURLException {
         baseClass.searchBarClick();
         baseClass.sendKeysToSearchBar(" ");
-        ((AndroidDriver)baseClass.driver).pressKeyCode(AndroidKeyCode.ENTER);
+        ((AndroidDriver)baseClass.driver).pressKeyCode(AndroidKeyCode.KEYCODE_SEARCH);
         baseClass.searchError("Oops! Something went wrong");
 
     }
